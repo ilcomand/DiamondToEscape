cf53c2e2c193c671a91352b4e70051b47c90348c
ILCOMAND-PC\ALe_CoMaNd.29 <alecomand.29@gmail.com>
2018-02-19 14:38:07 +0100

    Creato progetto per Test e creato oggetto Field

---
+++ CatchTheGold.Core.Tests/EroeTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatchTheGold.Core.Tests
+{
+    [TestFixture]
+    public class EroeTests
+    {
+
+    }
+}
---
+++ CatchTheGold.Core/Field.cs
@@ -0,0 +1,48 @@
+﻿namespace CatchTheGold.Core
+{
+    class Field
+    {
+        public static bool CheckWall(char[,] field, Direction direction, int x, int y)
+        {
+            bool f = false;
+
+            switch (direction)
+            {
+                case Direction.Up:
+                    {
+                        if (field[x, y - 1] == 'M')
+                            f = true;
+
+                        break;
+                    }
+
+                case Direction.Down:
+                    {
+                        if (field[x, y + 1] == 'M')
+                            f = true;
+
+                        break;
+                    }
+
+                case Direction.Right:
+                    {
+                        if (field[x + 1, y] == 'M')
+                            f = true;
+
+                        break;
+                    }
+
+                case Direction.Left:
+                    {
+                        if (field[x - 1, y] == 'M')
+                            f = true;
+
+                        break;
+                    }
+            }
+
+            if (f) return true;
+            else return false;
+        }
+    }
+}
