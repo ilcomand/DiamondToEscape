408ce47a21063f2d7425c2b588aa58e07dff820e
ILCOMAND-PC\ALe_CoMaNd.29 <alecomand.29@gmail.com>
2018-02-20 14:07:40 +0100

    Funziona ma il test dà problemi

--- CatchTheGold.Core.Tests/EroeTests.cs
+++ CatchTheGold.Core.Tests/EroeTests.cs
@@ -11,12 +11,11 @@ namespace CatchTheGold.Core.Tests
     public class EroeTests
     {
         [TestCase(Direction.Up, 2, 0, 2, 0)]
-        [TestCase(Direction.Up, 0, 1, 0, 0)]
+        [TestCase(Direction.Up, 1, 0, 1, 0)]
         [TestCase(Direction.Up, 1, 2, 1, 2)]
         [TestCase(Direction.Left, 0, 4, 0, 4)]
         [TestCase(Direction.Down, 1, 4, 1, 4)]
         [TestCase(Direction.Right, 2, 0, 2, 0)]
-        [TestCase(Direction.Right, 0, 1, 0, 1)]
 
         public void Move(Direction direction, int startX, int startY, int expectedX, int expectedY)
         {
--- CatchTheGold.Core/Field.cs
+++ CatchTheGold.Core/Field.cs
@@ -5,41 +5,24 @@ namespace CatchTheGold.Core
     public class Field
     {
         public static bool CheckWall(FieldElement[,] field, Direction direction, int x, int y)
-        {
-            bool move = false;
-
+        {     
             switch (direction)
             {
-                case Direction.Up:
-                    {
-                        if (y - 1 >= 0 && field[x, y - 1] != FieldElement.Wall) move = true;
-                        break;
-                    }
+                case Direction.Up:                 
+                    return ((y - 1) >= 0 && field[x, y - 1] != FieldElement.Wall);
 
-                case Direction.Down:
-                    {
-                        if (y + 1 <= (field.GetLength(0) - 1) && field[x, y + 1] != FieldElement.Wall) move = true;
-                        break;
-                    }
-
+                case Direction.Down:                    
+                    return ((y + 1) <= (field.GetLength(0) - 1) && field[x, y + 1] != FieldElement.Wall);  
+                                        
                 case Direction.Right:
-                    {
-                        if (x + 1 <= (field.GetLength(1) - 1) && field[x + 1, y] != FieldElement.Wall) move = true;
-                        break;
-                    }
+                    return ((x + 1) <= (field.GetLength(1) - 1) && field[x + 1, y] != FieldElement.Wall);
 
                 case Direction.Left:
-                    {
-                        if (x - 1 >= 0 && field[x - 1, y] != FieldElement.Wall) move = true;
-                        break;
-                    }
+                    return ((x - 1) >= 0 && field[x - 1, y] != FieldElement.Wall);
 
                 default:
                     throw new Exception("Invalid Direction !");
-            }
-
-            if (move) return true;
-            else return false;          
+            }       
         }
     }
 }
